cc7a77f1cc5ec67768862a1927e96c9000c2df78
Michael Lai <themichaellai@gmail.com>
2014-08-17 23:38:28 -0700

    Save last restarted time in redis

--- restarter.go
+++ restarter.go
@@ -4,11 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/bgentry/heroku-go"
+	"github.com/garyburd/redigo/redis"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/auth"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"time"
 )
+
+const redisLastRestartedKey string = "herokurestarter:lastrestarted"
 
 func check(e error) {
 	if e != nil {
@@ -26,6 +31,17 @@ func readSettings() map[string]interface{} {
 	return settingsJson
 }
 
+func saveTimestampRedis(redisClient redis.Conn) {
+	_, err := redisClient.Do("SET", redisLastRestartedKey, time.Now().Unix())
+	check(err)
+}
+
+func getTimestampRedis(redisClient redis.Conn) time.Time {
+	seconds, err := redis.Int64(redisClient.Do("GET", redisLastRestartedKey))
+	check(err)
+	return time.Unix(seconds, 0)
+}
+
 func getApps(client heroku.Client) {
 	if apps, err := client.AppList(nil); err != nil {
 		fmt.Println("Failed to get app list,", err)
@@ -40,6 +56,10 @@ func main() {
 	settings := readSettings()
 	heroku := heroku.Client{
 		Username: settings["user"].(string), Password: settings["api_key"].(string)}
+
+	redisClient, err := redis.Dial("tcp", ":6379")
+	check(err)
+
 	args := os.Args[1:]
 	if len(args) > 0 {
 		getApps(heroku)
@@ -51,13 +71,16 @@ func main() {
 	}))
 
 	m.Get("/", func() string {
-		return "help I've crashed and can't get up"
+		var time = getTimestampRedis(redisClient)
+		return fmt.Sprintf("help I've crashed and can't get up %s",
+			strconv.FormatInt(time.Unix(), 10))
 	})
 
 	m.Post("/restart", func() string {
 		if err := heroku.DynoRestartAll(settings["dyno_id"].(string)); err != nil {
 			panic(err)
 		}
+		go saveTimestampRedis(redisClient)
 		return "success"
 	})
 
